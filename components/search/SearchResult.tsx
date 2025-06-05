b2f43b08f00bde0ceba6c764140d4ab6d9284465
Guilherme Jose Pessato <gui_pessato@hotmail.com>
2025-06-05 15:45:11 -0300

    feat: PLP - Search and Catalog

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -76,12 +76,12 @@ function PageResult(props: SectionProps<typeof loader>) {
       >
         <a
           rel="prev"
-          class="btn btn-ghost"
+          class="w-full max-w-32 p-3 bg-[#3A4332] text-[#97A37F] h-8 flex items-center justify-center font-bold text-[14.06px] leading-[170%] tracking-[16%] hover:bg-[#293023] cursor-pointer transition"
           hx-swap="outerHTML show:parent:top"
           hx-get={partialPrev}
         >
           <span class="inline [.htmx-request_&]:hidden">
-            Show Less
+            Mostrar menos
           </span>
           <span class="loading loading-spinner hidden [.htmx-request_&]:block" />
         </a>
@@ -91,8 +91,8 @@ function PageResult(props: SectionProps<typeof loader>) {
         data-product-list
         class={clx(
           "grid items-center",
-          "grid-cols-2 gap-2",
-          "sm:grid-cols-4 sm:gap-10",
+          "grid-cols-2 gap-4", // 2 colunas no mobile, gap-4 = 16px
+          "sm:grid-cols-3", // 3 colunas a partir de sm (≥640px)
           "w-full",
         )}
       >
@@ -114,14 +114,14 @@ function PageResult(props: SectionProps<typeof loader>) {
               <a
                 rel="next"
                 class={clx(
-                  "btn btn-ghost",
+                  "w-full max-w-32 p-3 bg-[#3A4332] text-[#97A37F] h-8 flex items-center justify-center font-bold text-[14.06px] leading-[170%] tracking-[16%] hover:bg-[#293023] cursor-pointer transition",
                   (!nextPageUrl || partial === "hideMore") && "hidden",
                 )}
                 hx-swap="outerHTML show:parent:top"
                 hx-get={partialNext}
               >
                 <span class="inline [.htmx-request_&]:hidden">
-                  Show More
+                  Mostrar mais
                 </span>
                 <span class="loading loading-spinner hidden [.htmx-request_&]:block" />
               </a>
@@ -209,8 +209,9 @@ function Result(props: SectionProps<typeof loader>) {
     },
   });
   const results = (
-    <span class="text-sm font-normal">
-      {page.pageInfo.recordPerPage} of {page.pageInfo.records} results
+    <span class="text-md uppercase font-normal text-[#1F251C]">
+      {/* {page.pageInfo.recordPerPage} de {page.pageInfo.records} produtos encontrados */}
+      {page.pageInfo.records} produtos encontrados
     </span>
   );
   const sortBy = sortOptions.length > 0 && (
@@ -222,17 +223,17 @@ function Result(props: SectionProps<typeof loader>) {
         {partial
           ? <PageResult {...props} />
           : (
-            <div class="container flex flex-col gap-4 sm:gap-5 w-full py-4 sm:py-5 px-5 sm:px-0">
+            <div class="container max-w-[1180px] flex flex-col gap-4 sm:gap-5 w-full py-4 sm:py-5 px-5 sm:px-0">
               <Breadcrumb itemListElement={breadcrumb?.itemListElement} />
 
               {device === "mobile" && (
                 <Drawer
                   id={controls}
                   aside={
-                    <div class="bg-base-100 flex flex-col h-full divide-y overflow-y-hidden">
+                    <div class="bg-base-100 flex flex-col h-full w-full divide-y overflow-y-hidden">
                       <div class="flex justify-between items-center">
                         <h1 class="px-4 py-3">
-                          <span class="font-medium text-2xl">Filters</span>
+                          <span class="font-medium text-2xl">Filtro</span>
                         </h1>
                         <label class="btn btn-ghost" for={controls}>
                           <Icon id="close" />
@@ -244,24 +245,47 @@ function Result(props: SectionProps<typeof loader>) {
                     </div>
                   }
                 >
-                  <div class="flex sm:hidden justify-between items-end">
-                    <div class="flex flex-col">
-                      {results}
-                      {sortBy}
+                  <div class="flex sm:hidden flex-col items-start">
+                    <div class="flex">{results}</div>
+                    <div class="w-full flex justify-between items-center gap-4 mt-5">
+                      <div className="flex max-w-1/2 w-full">{sortBy}</div>
+                      <div className="flex max-w-1/2 w-full">
+                        <label
+                          class="w-full h-9 min-h9 max-h-9 p-0 rounded-none flex items-center justify-center gap-2 bg-[#c6cfba] text-[#323f2d] text-sm font-bold uppercase"
+                          for={controls}
+                        >
+                          <svg
+                            class="w-4 h-4"
+                            width="16px"
+                            height="16px"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <g id="Interface / Filter">
+                              <path
+                                id="Vector"
+                                d="M20 5.6001C20 5.04005 19.9996 4.75981 19.8906 4.5459C19.7948 4.35774 19.6423 4.20487 19.4542 4.10899C19.2403 4 18.9597 4 18.3996 4H5.59961C5.03956 4 4.75981 4 4.5459 4.10899C4.35774 4.20487 4.20487 4.35774 4.10899 4.5459C4 4.75981 4 5.04005 4 5.6001V6.33736C4 6.58195 4 6.70433 4.02763 6.81942C4.05213 6.92146 4.09263 7.01893 4.14746 7.1084C4.20928 7.20928 4.29591 7.29591 4.46875 7.46875L9.53149 12.5315C9.70443 12.7044 9.79044 12.7904 9.85228 12.8914C9.90711 12.9808 9.94816 13.0786 9.97266 13.1807C10 13.2946 10 13.4155 10 13.6552V18.411C10 19.2682 10 19.6971 10.1805 19.9552C10.3382 20.1806 10.5814 20.331 10.8535 20.3712C11.1651 20.4172 11.5487 20.2257 12.3154 19.8424L13.1154 19.4424C13.4365 19.2819 13.5966 19.2013 13.7139 19.0815C13.8176 18.9756 13.897 18.8485 13.9453 18.7084C14 18.5499 14 18.37 14 18.011V13.6626C14 13.418 14 13.2958 14.0276 13.1807C14.0521 13.0786 14.0926 12.9808 14.1475 12.8914C14.2089 12.7911 14.2947 12.7053 14.4653 12.5347L14.4688 12.5315L19.5315 7.46875C19.7044 7.2958 19.7904 7.20932 19.8523 7.1084C19.9071 7.01893 19.9482 6.92146 19.9727 6.81942C20 6.70551 20 6.58444 20 6.3448V5.6001Z"
+                                stroke="#323f2d"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              />
+                            </g>
+                          </svg>
+                          Filtro
+                        </label>
+                      </div>
                     </div>
-
-                    <label class="btn btn-ghost" for={controls}>
-                      Filters
-                    </label>
                   </div>
                 </Drawer>
               )}
 
-              <div class="grid place-items-center grid-cols-1 sm:grid-cols-[250px_1fr]">
+              <div class="grid grid-cols-1 sm:grid-cols-[275px_1fr] lg:gap-10">
                 {device === "desktop" && (
-                  <aside class="place-self-start flex flex-col gap-9">
-                    <span class="text-base font-semibold h-12 flex items-center">
-                      Filters
+                  <aside class="place-self-start flex flex-col gap-9 w-full">
+                    <span class="text-base font-medium h-12 flex items-center text-md text-[#1F251C]">
+                      Filtro
                     </span>
 
                     <Filters filters={filters} />
