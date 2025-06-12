1c2f52913a16fb2da6adc3132f0ea38080bc156f
decobot <yohanna.rodrigues@agenciam3.com>
2025-06-12 14:17:20 -0300

    fix: envio para o masterdata

--- sections/Footer/Footer.tsx
+++ sections/Footer/Footer.tsx
@@ -1,9 +1,8 @@
-import { useDevice, useScript } from "@deco/deco/hooks";
+import { useDevice } from "@deco/deco/hooks";
 import { type ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import Section from "../../components/ui/Section.tsx";
 import Newsletter from "../../islands/Newsletter.tsx";
-import { invoke } from "../../runtime.ts";
 
 /** @titleBy title */
 interface Item {
@@ -79,112 +78,10 @@ function Footer({
   lojas = { title: "", lojas: [], verMais: "/" },
 }: Props) {
   const device = useDevice();
-  const handleSubmit = async () => {
-    console.log("oiii");
-    const nameInput = document.getElementById(
-      "name",
-    ) as HTMLInputElement | null;
-    const emailInput = document.getElementById(
-      "email",
-    ) as HTMLInputElement | null;
-    const politicaInput = document.getElementById(
-      "politica",
-    ) as HTMLInputElement | null;
-
-    if (!nameInput || !emailInput || !politicaInput) {
-      alert("Não foi possível acessar os campos do formulário.");
-      return;
-    }
-
-    const name = nameInput.value.trim();
-    const emaill = emailInput.value.trim();
-    const politica = politicaInput.checked ? "aceito" : "não aceito";
-
-    if (!name || !emaill || politica !== "aceito") {
-      alert("Preencha todos os campos e aceite a política de privacidade.");
-      return;
-    }
-
-    try {
-      await invoke.vtex.actions.masterdata.createDocument({
-        data: { name, emaill, politica },
-        acronym: "NL",
-      });
-
-      alert("Cadastro realizado com sucesso!");
-      nameInput.value = "";
-      emailInput.value = "";
-      politicaInput.checked = false;
-    } catch (err) {
-      console.error("Erro ao enviar:", err);
-      alert("Erro ao cadastrar. Tente novamente.");
-    }
-  };
 
   return (
     <footer class="sm:px-0 mt-5 sm:mt-10" style={{ backgroundColor: "#fff" }}>
       <Newsletter />
-      <div class="w-full flex justify-center bg-[#94A57D] pb-[30px] pt-[30px] p-5">
-        <div class="flex flex-col max-w-[506px] lg:max-w-[738px] ">
-          <p class="text-[20px] lg:text-[22px] text-[#ffffff] text-center font-normal mb-[20px]">
-            RECEBA DESCONTOS E NOVIDADES DIRETAMENTE NO SEU E-MAIL
-          </p>
-          <div class="space-y-4">
-            <div class="flex flex-col lg:flex-row w-full gap-2 lg:items-end  items-center">
-              <div class="flex flex-col w-full lg:w-[40%] max-w-[300px]">
-                <label
-                  class="block text-[16px] font-normal text-[#ffffff] mb-1"
-                  for="name"
-                >
-                  INFORME SEU NOME
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  class="w-full px-3 py-2 border outline-none border-write text-[#ffffff] placeholder-white max-h-[36px] shadow-sm focus:outline-none focus:ring-2  bg-[#94A57D]"
-                  placeholder="Seu nome"
-                  required
-                />
-              </div>
-              <div class="flex flex-col  w-full lg:w-[40%] max-w-[300px]">
-                <label
-                  class="block text-[16px] font-normal text-[#ffffff] mb-1"
-                  for="email"
-                >
-                  INFORME SEU EMAIL
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="w-full px-3 py-2 border outline-none border-write text-[#ffffff] placeholder-white max-h-[36px] shadow-sm focus:outline-none focus:ring-2  bg-[#94A57D]"
-                  placeholder="seu@email.com"
-                  required
-                />
-              </div>
-              <button
-                type="button"
-                hx-on:click={useScript(handleSubmit)}
-                class="w-full text-[12px] text-white py-2 max-h-[36px] max-w-[210px] lg:max-w-[100px] bg-[#4B5941] flex justify-center items-center"
-              >
-                CADASTRAR
-              </button>
-            </div>
-
-            <label htmlFor="politica" class="flex flex-row gap-2 items-start">
-              <input type="checkbox" name="politica" id="politica" />
-              <p class="text-[12px] text-white">
-                {" "}
-                AO SE CADASTRAR VOCÊ IRÁ CONCORDAR COM A NOSSA POLÍTICA DE
-                PRIVACIDADE E PODERÁ ALTERAR OU CANCELAR A NEWSLETTER A QUALQUER
-                MOMENTO QUE VOCÊ DESEJAR. AQUI VOCÊ ECONOMIZA NAS SUAS COMPRAS E
-                NÃO RECEBE SPAM
-              </p>
-            </label>
-          </div>
-        </div>
-      </div>
       {device === "mobile"
         ? (
           ""
