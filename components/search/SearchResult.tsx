daab46402b7d086b8822db6aa63e5bb26d157610
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-08-12 09:48:12 -0300

    feat: ajusta search result

--- components/search/SearchResult.tsx
+++ components/search/SearchResult.tsx
@@ -35,9 +35,39 @@ export interface Props {
 
 function NotFound() {
   return (
-    <div class="w-full flex justify-center items-center py-10">
-      <span>Not Found!</span>
-    </div>
+    <>
+      <div className="container w-full flex justify-center items-center py-2 mt-8">
+        <div className="mb-8">
+          <div className="max-w-container w-full mx-auto px-4 flex flex-col items-center justify-center text-center py-12">
+            <h1 className="text-[25px] leading-[25px] font-bold mb-6 uppercase">
+              Oops! <br /> O item que você buscou não foi encontrado!
+            </h1>
+            <h2 className="uppercase mb-3">
+              Mas não se preocupe, tente novamente utilizando nossas dicas:
+            </h2>
+            <ul className="mb-5">
+              <li className="text-[13px] leading-[13px]">
+                <span className="text-lg inline-block mr-1">•</span> Verifique
+                se não há erro de digitação.
+              </li>
+              <li className="text-[13px] leading-[13px]">
+                <span className="text-lg inline-block mr-1">•</span> Tente
+                utilizar uma única palavra.
+              </li>
+              <li className="text-[13px] leading-[13px]">
+                <span className="text-lg inline-block mr-1">•</span> Tente
+                buscar por termos menos específicos e posteriormente use os
+                filtros da busca.
+              </li>
+              <li className="text-[13px] leading-[13px]">
+                <span className="text-lg inline-block mr-1">•</span> Procure
+                utilizar sinônimos ao termo desejado.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
@@ -372,12 +402,11 @@ function Result(props: SectionProps<typeof loader>) {
 }
 
 function SearchResult({ page, ...props }: SectionProps<typeof loader>) {
-  if (!page) {
+  if (!page || page.products.length === 0) {
     return <NotFound />;
   }
   return <Result {...props} page={page} />;
 }
-
 export const loader = (props: Props, req: Request) => {
   return {
     ...props,
