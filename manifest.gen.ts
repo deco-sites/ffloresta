112c65e060bd5203c54026247d142b88980462d0
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-10-08 15:00:04 -0300

    feat: add new our stores pages

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -28,46 +28,47 @@ import * as $$$$$$8 from "./sections/Content/Faq.tsx";
 import * as $$$$$$9 from "./sections/Content/Hero.tsx";
 import * as $$$$$$10 from "./sections/Content/Intro.tsx";
 import * as $$$$$$11 from "./sections/Content/Logos.tsx";
-import * as $$$$$$12 from "./sections/Content/PromoCountdownBanner.tsx";
-import * as $$$$$$13 from "./sections/Content/TrustvoxCarousel.tsx";
-import * as $$$$$$14 from "./sections/Content/VideoWithBackground.tsx";
-import * as $$$$$$15 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$16 from "./sections/Header/Header.tsx";
-import * as $$$$$$17 from "./sections/Images/Banner.tsx";
-import * as $$$$$$18 from "./sections/Images/BannerCategory.tsx";
-import * as $$$$$$19 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$20 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$21 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$22 from "./sections/Institucional/BannerInstitucional.tsx";
-import * as $$$$$$23 from "./sections/Institucional/NossaHistoria.tsx";
-import * as $$$$$$24 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$25 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$26 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$27 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$28 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$29 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$30 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$31 from "./sections/Product/ProductShelfWithBackground.tsx";
-import * as $$$$$$32 from "./sections/Product/ProductShelfWithBanner.tsx";
-import * as $$$$$$33 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$34 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$35 from "./sections/Product/TrustvoxCertificate.tsx";
-import * as $$$$$$36 from "./sections/Product/TrustvoxClickableRating.tsx";
-import * as $$$$$$37 from "./sections/Product/TrustvoxCollector.tsx";
-import * as $$$$$$38 from "./sections/Product/TrustvoxDebug.tsx";
-import * as $$$$$$39 from "./sections/Product/TrustvoxDebugStars.tsx";
-import * as $$$$$$40 from "./sections/Product/TrustvoxRatingWidget.tsx";
-import * as $$$$$$41 from "./sections/Product/TrustvoxReviews.tsx";
-import * as $$$$$$42 from "./sections/Product/TrustvoxSeal.tsx";
-import * as $$$$$$43 from "./sections/Product/TrustvoxStars.tsx";
-import * as $$$$$$44 from "./sections/Product/TrustvoxTest.tsx";
-import * as $$$$$$45 from "./sections/Product/TrustvoxWidget.tsx";
-import * as $$$$$$46 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$47 from "./sections/SelosSeguranca.tsx";
-import * as $$$$$$48 from "./sections/Session.tsx";
-import * as $$$$$$49 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$50 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$51 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$12 from "./sections/Content/OurStores.tsx";
+import * as $$$$$$13 from "./sections/Content/PromoCountdownBanner.tsx";
+import * as $$$$$$14 from "./sections/Content/TrustvoxCarousel.tsx";
+import * as $$$$$$15 from "./sections/Content/VideoWithBackground.tsx";
+import * as $$$$$$16 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$17 from "./sections/Header/Header.tsx";
+import * as $$$$$$18 from "./sections/Images/Banner.tsx";
+import * as $$$$$$19 from "./sections/Images/BannerCategory.tsx";
+import * as $$$$$$20 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$21 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$22 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$23 from "./sections/Institucional/BannerInstitucional.tsx";
+import * as $$$$$$24 from "./sections/Institucional/NossaHistoria.tsx";
+import * as $$$$$$25 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$26 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$27 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$28 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$29 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$30 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$31 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$32 from "./sections/Product/ProductShelfWithBackground.tsx";
+import * as $$$$$$33 from "./sections/Product/ProductShelfWithBanner.tsx";
+import * as $$$$$$34 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$35 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$36 from "./sections/Product/TrustvoxCertificate.tsx";
+import * as $$$$$$37 from "./sections/Product/TrustvoxClickableRating.tsx";
+import * as $$$$$$38 from "./sections/Product/TrustvoxCollector.tsx";
+import * as $$$$$$39 from "./sections/Product/TrustvoxDebug.tsx";
+import * as $$$$$$40 from "./sections/Product/TrustvoxDebugStars.tsx";
+import * as $$$$$$41 from "./sections/Product/TrustvoxRatingWidget.tsx";
+import * as $$$$$$42 from "./sections/Product/TrustvoxReviews.tsx";
+import * as $$$$$$43 from "./sections/Product/TrustvoxSeal.tsx";
+import * as $$$$$$44 from "./sections/Product/TrustvoxStars.tsx";
+import * as $$$$$$45 from "./sections/Product/TrustvoxTest.tsx";
+import * as $$$$$$46 from "./sections/Product/TrustvoxWidget.tsx";
+import * as $$$$$$47 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$48 from "./sections/SelosSeguranca.tsx";
+import * as $$$$$$49 from "./sections/Session.tsx";
+import * as $$$$$$50 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$51 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$52 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -92,46 +93,47 @@ const manifest = {
     "site/sections/Content/Hero.tsx": $$$$$$9,
     "site/sections/Content/Intro.tsx": $$$$$$10,
     "site/sections/Content/Logos.tsx": $$$$$$11,
-    "site/sections/Content/PromoCountdownBanner.tsx": $$$$$$12,
-    "site/sections/Content/TrustvoxCarousel.tsx": $$$$$$13,
-    "site/sections/Content/VideoWithBackground.tsx": $$$$$$14,
-    "site/sections/Footer/Footer.tsx": $$$$$$15,
-    "site/sections/Header/Header.tsx": $$$$$$16,
-    "site/sections/Images/Banner.tsx": $$$$$$17,
-    "site/sections/Images/BannerCategory.tsx": $$$$$$18,
-    "site/sections/Images/Carousel.tsx": $$$$$$19,
-    "site/sections/Images/ImageGallery.tsx": $$$$$$20,
-    "site/sections/Images/ShoppableBanner.tsx": $$$$$$21,
-    "site/sections/Institucional/BannerInstitucional.tsx": $$$$$$22,
-    "site/sections/Institucional/NossaHistoria.tsx": $$$$$$23,
-    "site/sections/Links/LinkTree.tsx": $$$$$$24,
-    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$25,
-    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$26,
-    "site/sections/Newsletter/Newsletter.tsx": $$$$$$27,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$28,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$29,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$30,
-    "site/sections/Product/ProductShelfWithBackground.tsx": $$$$$$31,
-    "site/sections/Product/ProductShelfWithBanner.tsx": $$$$$$32,
-    "site/sections/Product/SearchResult.tsx": $$$$$$33,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$34,
-    "site/sections/Product/TrustvoxCertificate.tsx": $$$$$$35,
-    "site/sections/Product/TrustvoxClickableRating.tsx": $$$$$$36,
-    "site/sections/Product/TrustvoxCollector.tsx": $$$$$$37,
-    "site/sections/Product/TrustvoxDebug.tsx": $$$$$$38,
-    "site/sections/Product/TrustvoxDebugStars.tsx": $$$$$$39,
-    "site/sections/Product/TrustvoxRatingWidget.tsx": $$$$$$40,
-    "site/sections/Product/TrustvoxReviews.tsx": $$$$$$41,
-    "site/sections/Product/TrustvoxSeal.tsx": $$$$$$42,
-    "site/sections/Product/TrustvoxStars.tsx": $$$$$$43,
-    "site/sections/Product/TrustvoxTest.tsx": $$$$$$44,
-    "site/sections/Product/TrustvoxWidget.tsx": $$$$$$45,
-    "site/sections/Product/Wishlist.tsx": $$$$$$46,
-    "site/sections/SelosSeguranca.tsx": $$$$$$47,
-    "site/sections/Session.tsx": $$$$$$48,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$49,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$50,
-    "site/sections/Theme/Theme.tsx": $$$$$$51,
+    "site/sections/Content/OurStores.tsx": $$$$$$12,
+    "site/sections/Content/PromoCountdownBanner.tsx": $$$$$$13,
+    "site/sections/Content/TrustvoxCarousel.tsx": $$$$$$14,
+    "site/sections/Content/VideoWithBackground.tsx": $$$$$$15,
+    "site/sections/Footer/Footer.tsx": $$$$$$16,
+    "site/sections/Header/Header.tsx": $$$$$$17,
+    "site/sections/Images/Banner.tsx": $$$$$$18,
+    "site/sections/Images/BannerCategory.tsx": $$$$$$19,
+    "site/sections/Images/Carousel.tsx": $$$$$$20,
+    "site/sections/Images/ImageGallery.tsx": $$$$$$21,
+    "site/sections/Images/ShoppableBanner.tsx": $$$$$$22,
+    "site/sections/Institucional/BannerInstitucional.tsx": $$$$$$23,
+    "site/sections/Institucional/NossaHistoria.tsx": $$$$$$24,
+    "site/sections/Links/LinkTree.tsx": $$$$$$25,
+    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$26,
+    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$27,
+    "site/sections/Newsletter/Newsletter.tsx": $$$$$$28,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$29,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$30,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$31,
+    "site/sections/Product/ProductShelfWithBackground.tsx": $$$$$$32,
+    "site/sections/Product/ProductShelfWithBanner.tsx": $$$$$$33,
+    "site/sections/Product/SearchResult.tsx": $$$$$$34,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$35,
+    "site/sections/Product/TrustvoxCertificate.tsx": $$$$$$36,
+    "site/sections/Product/TrustvoxClickableRating.tsx": $$$$$$37,
+    "site/sections/Product/TrustvoxCollector.tsx": $$$$$$38,
+    "site/sections/Product/TrustvoxDebug.tsx": $$$$$$39,
+    "site/sections/Product/TrustvoxDebugStars.tsx": $$$$$$40,
+    "site/sections/Product/TrustvoxRatingWidget.tsx": $$$$$$41,
+    "site/sections/Product/TrustvoxReviews.tsx": $$$$$$42,
+    "site/sections/Product/TrustvoxSeal.tsx": $$$$$$43,
+    "site/sections/Product/TrustvoxStars.tsx": $$$$$$44,
+    "site/sections/Product/TrustvoxTest.tsx": $$$$$$45,
+    "site/sections/Product/TrustvoxWidget.tsx": $$$$$$46,
+    "site/sections/Product/Wishlist.tsx": $$$$$$47,
+    "site/sections/SelosSeguranca.tsx": $$$$$$48,
+    "site/sections/Session.tsx": $$$$$$49,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$50,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$51,
+    "site/sections/Theme/Theme.tsx": $$$$$$52,
   },
   "actions": {
     "site/actions/minicart/submit.ts": $$$$$$$$$0,
