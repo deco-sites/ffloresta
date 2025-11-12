f64c3c65a70dbf23e39aa07aeaf6ab0036c3c3e0
PedroLucasAraujo <pedro.lucas@almahcomunicacao.com.br>
2025-11-12 17:48:16 -0300

    fix: quebra de flags com wrap

--- components/product/ProductCard/ProductCardFlags.tsx
+++ components/product/ProductCard/ProductCardFlags.tsx
@@ -15,24 +15,22 @@ function ProductFlags({ product, flagsConfig = [] }: Props) {
   if (flags.length === 0) return null;
 
   return (
-    <div class="absolute top-0 left-0 flex gap-2">
+    <div class="absolute top-0 left-0 flex flex-wrap gap-2">
       {flags.map((flag, index) =>
-        flag.type === "text"
-          ? (
-            <span
-              key={index}
-              class="text-xs h-[15px] flex items-center px-2 w-max"
-              style={{
-                color: flag.textColor || "#fff",
-                backgroundColor: flag.backgroundColor || "#000",
-              }}
-            >
-              {flag.text}
-            </span>
-          )
-          : flag.type === "image" && flag.image
-          ? <img key={index} src={flag.image} alt="" class="object-contain" />
-          : null
+        flag.type === "text" ? (
+          <span
+            key={index}
+            class="text-xs h-[15px] flex items-center px-2 w-max"
+            style={{
+              color: flag.textColor || "#fff",
+              backgroundColor: flag.backgroundColor || "#000",
+            }}
+          >
+            {flag.text}
+          </span>
+        ) : flag.type === "image" && flag.image ? (
+          <img key={index} src={flag.image} alt="" class="object-contain" />
+        ) : null
       )}
     </div>
   );
